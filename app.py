3b4aefaf88582df49dcd43e3542b37b83f49831a
Prasad7007 <prasadagalave0007@gmail.com>
2025-08-21 23:25:04 +0530

    Edited app.py

--- app.py
+++ app.py
@@ -92,4 +92,5 @@ def predict():
     return render_template("predict.html", title="Predict", form=form, output=message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
+
